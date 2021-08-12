ad119930ac4ce6ca7a9a486cb12727ace5b6a4f1
andreAguia <alataguia@gmail.com>
2021-08-12 16:25:20 -0300

    Informa quando um contrato deve ser reajustado

--- _classes/class.AlertaContrato.php
+++ _classes/class.AlertaContrato.php
@@ -100,10 +100,11 @@ class AlertaContrato {
         }
 
         ################################################################################
-        # Verifica se a data inicial do contrato é igual ou superior a data de publicação
-        # Pega os dados
+        # Pega os dados do contrato para os alertas a seguir
         $dados = $contrato->getDados($idContrato);
 
+        #####################        
+        # Verifica se a data inicial do contrato é igual ou superior a data de publicação        
         # analisa as datas
         if (!empty($dados["dtInicial"])) {
 
@@ -113,6 +114,43 @@ class AlertaContrato {
             if ($dtInicial <> $dtPublicacao) {
                 if (dataMaior($dtInicial, $dtPublicacao) == $dtPublicacao) {
                     $erro[] = "A data de Publicação NÃO pode ser posterior a data inicial!!";
+                }
+            }
+        }
+
+        #####################
+        # Verifica nos contratos de receita se falta 1 mês para o reajuste
+        # Verifica se é receita
+        $modalidade = new Modalidade();        
+        if ($modalidade->getTipo($dados["idModalidade"]) == "Receita") {
+            
+            # Verifica se foi informada a data da proposta
+            if (!empty($dados["dtProposta"])) {
+                # Pega a data da proposta
+                $dtProposta = date_to_php($dados["dtProposta"]);
+
+                # Transporta para o ano atual
+                while (year($dtProposta) < date("Y")):
+                    # Adiciona 1 ano
+                    $dtProposta = addAnos($dtProposta, 1);
+                endwhile;
+
+                # Verifica se estamos a menos de 30 dias do reajuste
+                $diferenca = abs(dataDif($dtProposta));
+                
+                if (dataDif($dtProposta) < 1 AND $diferenca < 30) {
+
+                    if ($diferenca > 1) {
+                        $erro[] = "Faltam {$diferenca} dias para a data de reajuste!! ({$dtProposta})";
+                    }
+                    
+                    if ($diferenca == 1) {
+                        $erro[] = "Falta 1 dia para a data de reajuste!! ({$dtProposta})";
+                    }
+                    
+                    if ($diferenca == 0) {
+                        $erro[] = "HOJE é o dia do reajuste!! ({$dtProposta})";
+                    }
                 }
             }
         }
@@ -127,13 +165,13 @@ class AlertaContrato {
 
                 $painel = new Callout("alert");
                 $painel->abre();
-                
+
                 $grid = new Grid();
                 $grid->abreColuna(1);
 
                 $figura = new Imagem(PASTA_FIGURAS_GERAIS . 'aviso.png', 'Alerta', 50, 50);
                 $figura->show();
-                
+
                 $grid->fechaColuna();
                 $grid->abreColuna(11);
 
@@ -142,7 +180,7 @@ class AlertaContrato {
                 foreach ($erro as $item) {
                     p($item, "palerta");
                 }
-                
+
                 $grid->fechaColuna();
                 $grid->fechaGrid();
                 $painel->fecha();
