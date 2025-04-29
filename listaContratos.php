ee8ec26aa8032af8b3e3b4bdabc2548c2a41bdb7
André Águia <alataguia@gmail.com>
2025-04-29 11:50:29 -0300

    teste de listagem

--- listaContratos.php
+++ listaContratos.php
@@ -1,7 +1,9 @@
 <?php
 
 # Configuração
-include ("/sistema/_config.php");
+#include ("/sistema/_config.php");
 
 # Cabeçalho da Página
-AreaServidor::cabecalho();
+#AreaServidor::cabecalho();
+
+echo "olá";
